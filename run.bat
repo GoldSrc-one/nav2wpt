766c539dd521a687fbec68384da98ac6e75dab2b
Jonatan Matějka <jonatan1024@gmail.com>
2026-03-04 08:58:46 +0100

    marinebot waypoints

--- run.bat
+++ run.bat
@@ -1,10 +1,12 @@
+bin\Debug\net8.0\nav2wpt D:\hlds\czero\maps D:\hlds\czero\maps foxbot waypoints\foxbot
 bin\Debug\net8.0\nav2wpt D:\hlds\czero\maps D:\hlds\cstrike\maps foxbot waypoints\foxbot
 bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\cstrike\maps foxbot waypoints\foxbot
 bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\valve\maps foxbot waypoints\foxbot
 bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\gearbox\maps foxbot waypoints\foxbot
 bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\dmc\maps foxbot waypoints\foxbot
-bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\tfc\maps foxbot waypoints\foxbot
+bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\dod\maps foxbot waypoints\foxbot
 
+bin\Debug\net8.0\nav2wpt D:\hlds\czero\maps D:\hlds\czero\maps marine_bot waypoints\marine_bot
 bin\Debug\net8.0\nav2wpt D:\hlds\czero\maps D:\hlds\cstrike\maps marine_bot waypoints\marine_bot
 bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\cstrike\maps marine_bot waypoints\marine_bot
 bin\Debug\net8.0\nav2wpt D:\hlds\cstrike\maps D:\hlds\valve\maps marine_bot waypoints\marine_bot
